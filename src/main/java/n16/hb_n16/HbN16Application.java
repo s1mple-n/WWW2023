6824fc32609532352b61f54c54df957565e2b590
s1mple-n <minhhuy123nguyen@gmail.com>
2023-05-07 22:08:06 +0700

    start project

---
+++ src/main/java/n16/hb_n16/HbN16Application.java
@@ -0,0 +1,13 @@
+package n16.hb_n16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbN16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbN16Application.class, args);
+    }
+
+}
---
+++ src/test/java/n16/hb_n16/HbN16ApplicationTests.java
@@ -0,0 +1,13 @@
+package n16.hb_n16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbN16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
