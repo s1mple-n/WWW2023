23d05950c782e76841c4e1388382b6b9f5d22a27
Phatjiro <nguyenvanphat911@gmail.com>
2023-05-14 16:15:20 +0700

    add ProductController, PA and PAOrderController, html for controller

--- src/main/java/n16/hb_n16/impl/SaleOrderDetailServiceImpl.java
+++ src/main/java/n16/hb_n16/impl/SaleOrderDetailServiceImpl.java
@@ -24,7 +24,7 @@ public class SaleOrderDetailServiceImpl implements SaleOrderDetailService {
     }
 
     @Override
-    public List<SaleOrderDetail> getAllSaleOrderDetailsBySaleOrderID(int saleOrderID) {
+    public List<SaleOrderDetail> getSaleOrderDetailsBySaleOrderID(int saleOrderID) {
         return saleOrderDetailRepository.getSaleOrderDetailsBySaleOrderID(saleOrderID);
     }
 
--- src/main/java/n16/hb_n16/services/SaleOrderDetailService.java
+++ src/main/java/n16/hb_n16/services/SaleOrderDetailService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface SaleOrderDetailService {
     List<SaleOrderDetail> getAllSaleOrderDetail();
-    List<SaleOrderDetail> getAllSaleOrderDetailsBySaleOrderID(int saleOrderID);
+    List<SaleOrderDetail> getSaleOrderDetailsBySaleOrderID(int saleOrderID);
     List<SaleOrderDetail> getSaleOrderDetailsByBagID(int bagID);
     SaleOrderDetail getSaleOrderDetailBySaleOrderIdAndBagId(int saleOrderID, int bagID);
     void addOrUpdateSaleOrderDetail(SaleOrderDetail saleOrderDetail);
